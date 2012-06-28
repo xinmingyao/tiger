383eb20e5b2bc4b5dfe2ab7e4f005232630b2415
xinming.yao <yaoxinming@gmail.com>
2012-06-28 14:25:32 +0800

    init project

---
+++ src/tiger_app.erl
@@ -0,0 +1,16 @@
+-module(tiger_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    tiger_sup:start_link().
+
+stop(_State) ->
+    ok.
