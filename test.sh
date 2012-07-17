79145442900d9bfd224e3f88891a46c6b552cdeb
xinming.yao <yaoxinming@gmail.com>
2012-07-17 15:11:06 +0800

    add api and integration test for tiger

---
+++ test.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# /bin/sh on Solaris is not a POSIX compatible shell, but /usr/bin/ksh is.
+if [ `uname -s` = 'SunOS' -a "${POSIX_SHELL}" != "true" ]; then
+    POSIX_SHELL="true"
+    export POSIX_SHELL
+    exec /usr/bin/ksh $0 $@
+fi
+unset POSIX_SHELL # clear it so if we invoke other scripts, they run as ksh as well
+
+
+
+BASEDIR="$PWD"
+
+# detecting gmake and if exists use it
+# if not use make
+# (code from github.com/tuncer/re2/c_src/build_deps.sh
+which gmake 1>/dev/null 2>/dev/null && MAKE=gmake
+MAKE=${MAKE:-make}
+
+# Changed "make" to $MAKE
+
+case "$1" in
+
+    test)
+	
+        if [ ! -d dev ]; then
+            (make && make devrel) 
+        fi
+        if [ ! -d test-tiger ]; then
+            git clone git://github.com/xinmingyao/test-tiger 
+        fi
+        (cd test-tiger && prove t )
+        ;;
+
+esac
+
